356d6fba920022fd4b4eb54e8ef23f4c4b0c2f8b
vin <hsiaovin@yeah.net>
2019-10-15 15:50:57 +0800

    test commit 1

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
 
 int main()
 {
+	cout << "Hello World!" << endl;
+	cout << 1 + 2 << endl;
+	system("pause");
     return 0;
 }
-
